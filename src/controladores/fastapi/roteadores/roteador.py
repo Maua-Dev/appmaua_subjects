379c38db75f97541a413aed7c0c187de7161085d
VitorMartin <vitor.martin.simoni@gmail.com>
2021-10-06 23:49:28 -0300

    Removendo tipagem

--- src/controladores/fastapi/roteadores/roteador.py
+++ src/controladores/fastapi/roteadores/roteador.py
@@ -2,12 +2,11 @@ from fastapi import APIRouter
 
 from .rotas.rotas_mss_info import RotasMssInfo
 from .rotas.rotas_materias import RotasMaterias
-from src.fabricas.controladores.fastapi.fabrica_controlador_fastapi import FabricaControladorFastapi
 
 
 class Roteador(APIRouter):
 
-    def __init__(self, _ctrl: FabricaControladorFastapi):
+    def __init__(self, _ctrl):
 
         super().__init__()
 
