1b30c3b45cc22844872fd298d5d92470d9f59cd5
Bruno Vilardi Bueno <62917140+Brvilardi@users.noreply.github.com>
2022-01-23 21:51:32 -0300

    update domain name

--- src/server.py
+++ src/server.py
@@ -4,4 +4,4 @@ from src.main.main import app
 from mangum import Mangum
 
 
-handler = Mangum(app,api_gateway_endpoint_url='subjects')
+handler = Mangum(app,api_gateway_endpoint_url='appmaua_subjects')
