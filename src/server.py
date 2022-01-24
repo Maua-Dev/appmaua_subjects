585019c94fa06a27489ccf92c7744b7a01696e2a
Bruno Vilardi Bueno <62917140+Brvilardi@users.noreply.github.com>
2022-01-23 22:03:46 -0300

    Update server.py

--- src/server.py
+++ src/server.py
@@ -4,4 +4,5 @@ from src.main.main import app
 from mangum import Mangum
 
 
-handler = Mangum(app,api_gateway_endpoint_url='appmaua_subjects')
+handler = Mangum(app,
+                 api_gateway_base_path='appmaua_subjects')
