30934833d439e62037e4908afb5705eaacbdd612
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2022-01-24 20:30:51 -0300

    clen merge

--- src/repositorios/erros/erro_materia_nao_encontrada.py
+++
@@ -1,3 +0,0 @@
-class ErroMateriaNaoEncontrada(Exception):
-    def __init__(self):
-        super().__init__("A matéria requisitada não foi encontrada")
--- src/repositorios/erros/erro_materia_nao_encontrada_pelo_id_professor.py
+++
@@ -1,3 +0,0 @@
-class ErroMateriaNaoEncontradaPeloIDProfessor(Exception):
-    def __init__(self, id: str):
-        super().__init__(f"Não foram encontradas matérias para o professor de ID = {id}")
