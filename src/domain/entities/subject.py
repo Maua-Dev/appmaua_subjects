fd4b96663636ef18cb9a57409a628eab0b56b92b
Renan Reschke <renanreschke@hotmail.com>
2022-01-26 17:24:09 -0300

    Validators e alguns atributos

--- src/domain/entities/subject.py
+++ src/domain/entities/subject.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from pydantic.main import BaseModel
 
@@ -6,19 +6,33 @@ from pydantic import validator
 
 from src.domain.errors.errors import EntityError
 
+
 class Subject(BaseModel):    
     codeSubject: str
     name: str
-    
-    
+    idProfessor: int
+    idStudents: List[int]
 
     @validator('codeSubject')
     def codeSubject_is_not_empty(cls,v: str) -> str:
-        if len(v) == 0:
+        if len(v) == 0 or v is None:
             raise EntityError('CodeSubject')
         return v
+
     @validator('name')
     def name_is_not_empty(cls,v: str)-> str:
-        if len(v) == 0:
+        if len(v) == 0 or v is None:
             raise EntityError('Name')
+        return v
+
+    @validator('idProfessor')
+    def idprofessor_is_not_empty(cls, v: int) -> int:
+        if v == 0 or v is None:
+            raise EntityError('idProfessor')
+        return v
+
+    @validator('idStudents')
+    def idstudents_is_not_null(cls, v: List) -> List:
+        if v is None:
+            raise EntityError('idStudents')
         return v
