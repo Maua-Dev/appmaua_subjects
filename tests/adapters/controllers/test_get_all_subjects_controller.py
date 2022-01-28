38ef8ebaf07b0bd123bb63bc5632cf082962b68b
Renan Reschke <renanreschke@hotmail.com>
2022-01-28 17:06:06 -0300

    refat

--- tests/adapters/controllers/test_get_all_subjects_controller.py
+++ tests/adapters/controllers/test_get_all_subjects_controller.py
@@ -1,14 +1,16 @@
-from src.adapters.controllers.get_all_subjects_controller import GetStudentSubjectsController
+from src.adapters.controllers.get_all_subjects_controller import GetAllSubjectsController
 from src.infra.repositories.subject_repository_mock import SubjectRepositoryMock
 from src.adapters.helpers.http_models import HttpRequest
 
-class Test_GetAllSubjectsController():
+class Test_GetAllSubjectsController:
 
-    getStudentSubjectsController = GetStudentSubjectsController(SubjectRepositoryMock())
-    req = HttpRequest(query=None)
-    answer = getStudentSubjectsController(req)
+    def test_get_all_subjects_controller(self):
 
-    assert type(answer.body) is dict
-    assert type(answer.body['subjects']) is list
-    assert len(answer.body['subjects']) == 5
-    assert answer.body['count'] == 5
+        getAllSubjectsController = GetAllSubjectsController(SubjectRepositoryMock())
+        req = HttpRequest(query=None)
+        answer = getAllSubjectsController(req)
+
+        assert type(answer.body) is dict
+        assert type(answer.body['subjects']) is list
+        assert len(answer.body['subjects']) == 5
+        assert answer.body['count'] == 5
