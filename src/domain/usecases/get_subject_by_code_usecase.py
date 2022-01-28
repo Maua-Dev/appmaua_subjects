97e4ed4fcde3ed8e0490b59c99a461a8649cad86
Renan Reschke <renanreschke@hotmail.com>
2022-01-28 15:01:31 -0300

    correção

--- src/domain/usecases/get_subject_by_code_usecase.py
+++ src/domain/usecases/get_subject_by_code_usecase.py
@@ -4,16 +4,16 @@ from src.domain.repositories.subject_repository_interface import ISubjectReposit
 from src.domain.entities.subject import Subject
 
 
-class GetSubjectByIdUsecase:
+class GetSubjectByCodeUsecase:
 
     def __init__(self, subjectRepository: ISubjectRepository) -> None:
         self._subjectRepository = subjectRepository
 
-    def __call__(self, idSubject: int) -> List[Subject]:
+    def __call__(self, codeSubject: str) -> Subject:
         try:
-            if idSubject is None:
+            if codeSubject is None:
                 raise Exception('idSubject is None')
-            subjects = self._subjectRepository.getSubjectById(idSubject=idSubject)
+            subjects = self._subjectRepository.getSubjectByCode(codeSubject=codeSubject)
             return subjects
         except Exception as error:
             raise UnexpectedError('GetSubjectById', str(error))
