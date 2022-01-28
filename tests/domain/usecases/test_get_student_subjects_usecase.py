77c9e2aee2f79746cdd58ff0b2805fb77704dd0f
Renan Reschke <renanreschke@hotmail.com>
2022-01-28 16:02:32 -0300

    refat

--- tests/domain/usecases/test_get_student_subjects_usecase.py
+++ tests/domain/usecases/test_get_student_subjects_usecase.py
@@ -1,4 +1,6 @@
 import pytest
+
+from src.domain.entities.subject import Subject
 from src.domain.usecases.get_student_subjects_usecase import GetStudentSubjectsUsecase
 from src.domain.errors.errors import UnexpectedError
 from src.infra.repositories.subject_repository_mock import SubjectRepositoryMock
@@ -10,6 +12,8 @@ class Test_GetStudentSubjectsUsecase:
         getStudentSubjectsUsecase = GetStudentSubjectsUsecase(subjectRepository=SubjectRepositoryMock())
         subjects = getStudentSubjectsUsecase(1)
         assert len(subjects) > 0
+        assert Subject(id=1, codeSubject='ECM501', name='Ciencia de dados') in subjects
+        assert Subject(id=2, codeSubject='ECM502', name='Devops') in subjects
 
     def test_get_student_subjects_empty(self):
         getStudentSubjectsUsecase = GetStudentSubjectsUsecase(subjectRepository=SubjectRepositoryMock())
