3d77da332f5ec9edc0bcbae1e68a185c5e128cb1
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-01-31 16:19:08 -0300

    test controller get by professor id

---
+++ src/adapters/controllers/get_subject_by_professor_id_controller.py
@@ -0,0 +1,33 @@
+from src.adapters.errors.http_exception import HttpException
+from src.domain.errors.errors import UnexpectedError, NoItemsFound
+from src.domain.repositories.subject_repository_interface import ISubjectRepository
+from src.domain.usecases.get_subject_by_professor_id_usecase import GetSubjectByProfessorIdUsecase
+from src.adapters.helpers.http_models import BadRequest, HttpRequest, HttpResponse, InternalServerError, Ok, NoContent
+
+
+class GetSubjectByProfessorIdController:
+    def __init__(self, subjectRepository: ISubjectRepository) -> None:
+        self._getSubjectByProfessorIdUsecase = GetSubjectByProfessorIdUsecase(subjectRepository)
+
+    def __call__(self, req: HttpRequest) -> HttpResponse:
+        try:
+            if req.query['idProfessor'] is None:
+                return BadRequest('idProfessor is null.')
+
+            if type(req.query['idProfessor']) is not int:
+                return BadRequest('idProfessor must be int.')
+
+            idProfessor = req.query['idProfessor']
+
+            subjects, count = self._getSubjectByProfessorIdUsecase(idProfessor)
+
+            response = {"subjects": subjects, "count": count}
+
+            return Ok(response)
+
+        except NoItemsFound:
+            return NoContent()
+
+        except UnexpectedError as e:
+            err = InternalServerError(e.message)
+            return HttpException(message=err.body, status_code=err.status_code)
---
+++ tests/adapters/controllers/test_get_subject_by_professor_id_controller.py
@@ -0,0 +1,33 @@
+from src.adapters.controllers.get_subject_by_professor_id_controller import GetSubjectByProfessorIdController
+from src.infra.repositories.subject_repository_mock import SubjectRepositoryMock
+from src.adapters.helpers.http_models import HttpRequest, NoContent, BadRequest
+
+
+class Test_GetSubjectByProfessorIdController:
+
+    def test_get_subject_by_professor_id_controller(self):
+
+        getSubjectByProfessorIdController = GetSubjectByProfessorIdController(SubjectRepositoryMock())
+        req = HttpRequest(query={'idProfessor': 1})
+        answer = getSubjectByProfessorIdController(req)
+
+        assert type(answer.body) is dict
+        assert type(answer.body['subjects']) is list
+        assert answer.body['count'] == 3
+        assert answer.status_code == 200
+
+    def test_get_subject_by_professor_id_controller_no_item_found_id_3(self):
+        getSubjectByProfessorIdController = GetSubjectByProfessorIdController(SubjectRepositoryMock())
+        req = HttpRequest(query={'idProfessor': 3})
+        answer = getSubjectByProfessorIdController(req)
+
+        assert type(answer) is NoContent
+        assert answer.status_code == 204
+
+    def test_get_subject_by_professor_id_controller_error(self):
+        getSubjectByProfessorIdController = GetSubjectByProfessorIdController(SubjectRepositoryMock())
+        req = HttpRequest(query={'idProfessor': "1"})
+        answer = getSubjectByProfessorIdController(req)
+
+        assert type(answer) is BadRequest
+        assert answer.status_code == 400
