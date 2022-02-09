5d565fcff3ff33bcad0ad38c4d58fad8172fe400
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2022-02-09 00:47:12 -0300

    remove testmain

--- tests/adapters/controllers/test_get_subject_by_code_usecase_controller.py
+++ tests/adapters/controllers/test_get_subject_by_code_usecase_controller.py
@@ -1,3 +1,4 @@
+import pytest
 from src.adapters.controllers.get_subject_by_code_controller import GetSubjectByCodeController
 from src.domain.usecases.get_subject_by_code_usecase import GetSubjectByCodeUsecase
 from src.infra.repositories.subject_repository_mock import SubjectRepositoryMock
@@ -6,26 +7,29 @@ from src.domain.entities.subject import Subject
 
 class Test_GetSubjectByCodeController:
 
-    def test_get_subject_by_code_controller(self):
+    @pytest.mark.asyncio
+    async def test_get_subject_by_code_controller(self):
         getSubjectByCodeController = GetSubjectByCodeController(getSubjectByCodeUsecase=GetSubjectByCodeUsecase(subjectRepository=SubjectRepositoryMock()))
         req = HttpRequest(query={'codeSubject': 'ecm505'})
-        answer = getSubjectByCodeController(req)
+        answer = await getSubjectByCodeController(req)
         
         assert type(answer.body) is Subject        
         assert answer.status_code == 200
 
-    def test_get_subject_by_code_controller_no_item_found(self):
+    @pytest.mark.asyncio
+    async def test_get_subject_by_code_controller_no_item_found(self):
         getSubjectByCodeController = GetSubjectByCodeController(getSubjectByCodeUsecase=GetSubjectByCodeUsecase(subjectRepository=SubjectRepositoryMock()))
         req = HttpRequest(query={'codeSubject': 'esm505'})
-        answer = getSubjectByCodeController(req)
+        answer = await getSubjectByCodeController(req)
 
         assert type(answer) is NoContent
         assert answer.status_code == 204
 
-    def test_get_subject_by_code_controller_error(self):
+    @pytest.mark.asyncio
+    async def test_get_subject_by_code_controller_error(self):
         getSubjectByCodeController = GetSubjectByCodeController(getSubjectByCodeUsecase=GetSubjectByCodeUsecase(subjectRepository=SubjectRepositoryMock()))
         req = HttpRequest(query={'codeSubject': 2})
-        answer = getSubjectByCodeController(req)
+        answer = await getSubjectByCodeController(req)
 
         assert type(answer) is BadRequest
         assert answer.status_code == 400
--- tests/main/test_main.py
+++ tests/main/test_main.py
@@ -1,17 +1,8 @@
 from fastapi.testclient import TestClient
-from fastapi import status, FastAPI, Request, Response
-from starlette.middleware.cors import CORSMiddleware
-from fastapi.responses import PlainTextResponse
 
-from src.adapters.controllers.get_all_subjects_controller import GetAllSubjectsController
-from src.adapters.controllers.get_student_subjects_controller import GetStudentSubjectsController
-from src.adapters.controllers.get_subject_by_code_controller import GetSubjectByCodeController
-from src.adapters.controllers.get_subject_by_professor_id_controller import GetSubjectByProfessorIdController
-from src.adapters.errors.http_exception import HttpException
-from src.adapters.helpers.http_models import HttpRequest
 from src.envs import EnvEnum, Envs
-from src.main.helpers.status import status as st
-from src.main.subjects.module import Modular
+from src.main.helpers.status import status
+
 from src.main.main import app
 Envs.appEnv = EnvEnum.MOCK
 # app = FastAPI()
@@ -64,111 +55,105 @@ Envs.appEnv = EnvEnum.MOCK
 #     response.status_code = st.get(result.status_code)
 #     return result
 
-client = TestClient(app)
+# client = TestClient(app)
 
 
-def test_read_all_subjects():
-    response = client.get("/")
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {
-                                  "status_code": 200,
-                                  "body": {
-                                    "subjects": [
-                                      {
-                                        "codeSubject": "ECM501",
-                                        "name": "Ciencia de dados"
-                                      },
-                                      {
-                                        "codeSubject": "ECM502",
-                                        "name": "Devops"
-                                      },
-                                      {
-                                        "codeSubject": "ECM504",
-                                        "name": "IA"
-                                      },
-                                      {
-                                        "codeSubject": "ECM505",
-                                        "name": "Banco de dados"
-                                      },
-                                      {
-                                        "codeSubject": "ECM503",
-                                        "name": "Controladores"
-                                      }
-                                    ],
-                                    "count": 5
-                                  }
-                                }
+# def test_read_all_subjects():
+#     response = client.get("/")
+#     assert response.status_code == status.HTTP_200_OK
+#     assert response.json() == {
+#                                     "subjects": [
+#                                       {
+#                                         "codeSubject": "ECM501",
+#                                         "name": "Ciencia de dados"
+#                                       },
+#                                       {
+#                                         "codeSubject": "ECM502",
+#                                         "name": "Devops"
+#                                       },
+#                                       {
+#                                         "codeSubject": "ECM504",
+#                                         "name": "IA"
+#                                       },
+#                                       {
+#                                         "codeSubject": "ECM505",
+#                                         "name": "Banco de dados"
+#                                       },
+#                                       {
+#                                         "codeSubject": "ECM503",
+#                                         "name": "Controladores"
+#                                       }
+#                                     ],
+#                                     "count": 5
+#                                   }
 
 
-def test_read_student_subjects():
-    response = client.get("/student/1")
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == [
-                                {
-                                  "codeSubject": "ECM501",
-                                  "name": "Ciencia de dados"
-                                },
-                                {
-                                  "codeSubject": "ECM502",
-                                  "name": "Devops"
-                                }
-                              ]
+# def test_read_student_subjects():
+#     response = client.get("/student/1")
+#     assert response.status_code == status.HTTP_200_OK
+#     assert response.json() == [
+#                                 {
+#                                   "codeSubject": "ECM501",
+#                                   "name": "Ciencia de dados"
+#                                 },
+#                                 {
+#                                   "codeSubject": "ECM502",
+#                                   "name": "Devops"
+#                                 }
+#                               ]
 
 
-def test_read_subject_by_code():
-    response = client.get("/subject/ecm505")
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {
-                                "codeSubject": "ECM505",
-                                "name": "Banco de dados"
-                              }
+# def test_read_subject_by_code():
+#     response = client.get("/subject/ecm505")
+#     assert response.status_code == status.HTTP_200_OK
+#     assert response.json() == {
+#                                 "codeSubject": "ECM505",
+#                                 "name": "Banco de dados"
+#                               }
 
 
-def test_read_subject_by_professor_id():
-    response = client.get("/professor/1")
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {
-                                  "status_code": 200,
-                                  "body": {
-                                    "subjects": [
-                                      {
-                                        "codeSubject": "ECM501",
-                                        "name": "Ciencia de dados"
-                                      },
-                                      {
-                                        "codeSubject": "ECM502",
-                                        "name": "Devops"
-                                      },
-                                      {
-                                        "codeSubject": "ECM504",
-                                        "name": "IA"
-                                      }
-                                    ],
-                                    "count": 3
-                                  }
-                                }
+# def test_read_subject_by_professor_id():
+#     response = client.get("/professor/1")
+#     assert response.status_code == status.HTTP_200_OK
+#     assert response.json() == {
+#                                     "subjects": [
+#                                       {
+#                                         "codeSubject": "ECM501",
+#                                         "name": "Ciencia de dados"
+#                                       },
+#                                       {
+#                                         "codeSubject": "ECM502",
+#                                         "name": "Devops"
+#                                       },
+#                                       {
+#                                         "codeSubject": "ECM504",
+#                                         "name": "IA"
+#                                       }
+#                                     ],
+#                                     "count": 3
+#                                   }
 
 
-def test_read_student_subjects_no_content():
-    response = client.get("/student/10")
-    assert response.status_code == status.HTTP_204_NO_CONTENT
+# def test_read_student_subjects_no_content():
+#     response = client.get("/student/10")
+#     assert response.status_code == status.HTTP_204_NO_CONTENT
 
 
-def test_read_subject_by_code_no_content():
-    response = client.get("/subject/ecm5050")
-    assert response.status_code == status.HTTP_204_NO_CONTENT
+# def test_read_subject_by_code_no_content():
+#     response = client.get("/subject/ecm5050")
+#     assert response.status_code == status.HTTP_204_NO_CONTENT
 
 
-def test_read_subject_by_professor_id_no_content():
-    response = client.get("/professor/10")
-    assert response.status_code == status.HTTP_204_NO_CONTENT
+# def test_read_subject_by_professor_id_no_content():
+#     response = client.get("/professor/10")
+#     assert response.status_code == status.HTTP_204_NO_CONTENT
 
 
-def test_read_student_subjects_bad_request():
-    response = client.get("/student/0")
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
+# def test_read_student_subjects_bad_request():
+#     response = client.get("/student/0")
+#     assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
-def test_read_subject_by_professor_id_bad_request():
-    response = client.get("/professor/0")
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
+# def test_read_subject_by_professor_id_bad_request():
+#     response = client.get("/professor/0")
+#     assert response.status_code == status.HTTP_400_BAD_REQUEST
