339c038b736abef199bdab512617462662b87677
Renan Reschke <renanreschke@hotmail.com>
2022-02-24 11:17:22 -0300

    correção uc

--- src/domain/usecases/get_num_students_by_grades.py
+++ src/domain/usecases/get_num_students_by_grades.py
@@ -1,7 +1,5 @@
-from typing import List
 from src.domain.errors.errors import UnexpectedError, NoItemsFound
 from src.domain.repositories.subject_repository_interface import ISubjectRepository
-from src.domain.entities.subject import Subject
 
 
 class GetNumStudensByGrades:
@@ -9,21 +7,21 @@ class GetNumStudensByGrades:
     def __init__(self, subjectRepository: ISubjectRepository) -> None:
         self._subjectRepository = subjectRepository
 
-    def __call__(self, value:float, codeSubject: str) -> int:
+    def __call__(self, gradeValue: float, codeSubject: str) -> int:
         try:
-            if codeSubject is None and value is None:
+            if codeSubject is None and gradeValue is None:
                 raise Exception('idSubject and value is None')
             elif codeSubject is None:
                 raise Exception('idSubject is None')
-            elif value is None:
+            elif gradeValue is None:
                 raise Exception('value is None')
 
-            numStudents = self._subjectRepository.getNumStudentsByGrades(value, codeSubject)
+            numStudents = self._subjectRepository.getNumStudentsByGrades(gradeValue, codeSubject)
 
             return numStudents
 
         except NoItemsFound:
-            raise NoItemsFound('GetAllSubjects')
+            raise NoItemsFound('GetNumStudensByGrades')
 
         except Exception as error:
             raise UnexpectedError('GetNumStudentsByGrade', str(error))
