4e9335316620d150aa1bafe66cb8216e67614044
Renan Reschke <renanreschke@hotmail.com>
2022-02-24 11:06:48 -0300

    correção classe degree

--- src/domain/entities/degree.py
+++ src/domain/entities/degree.py
@@ -1,24 +1,21 @@
 from typing import List
-from pydantic.class_validators import validator
-from pydantic.main import BaseModel
+from pydantic import validator, BaseModel
 
 from src.domain.errors.errors import EntityError
-
-from src.domain.entities.subject import Subject
 
 
 class Degree(BaseModel):
     name: str
-    subjects: List[Subject]
+    subjects: List[str]  # codeSubject
     duration: int
     idDegree: int
-    
+
     @validator('name')
     def name_is_not_empty(cls, v: str) -> str:
         if len(v) == 0:
             raise EntityError('Name')
         return v
-    
+
     @validator('duration')
     def duration_is_not_empty(cls, v: int) -> int:
         if v == 0:
@@ -26,7 +23,7 @@ class Degree(BaseModel):
         return v
 
     @validator('subjects')
-    def subjects_not_empty(cls, v: List[Subject]) -> List[Subject]:
+    def subjects_not_empty(cls, v: List[str]) -> List[str]:
         if len(v) == 0:
             raise EntityError('Subjects')
         return v
