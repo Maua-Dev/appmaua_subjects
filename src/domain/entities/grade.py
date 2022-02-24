b4bbb361315a6eb9e2ff4f12f43155f3717caf33
Renan Reschke <renanreschke@hotmail.com>
2022-02-24 14:10:40 -0300

    mudança nos parâmetros de nota; add tipo de nota (enum)

--- src/domain/entities/grade.py
+++ src/domain/entities/grade.py
@@ -1,30 +1,18 @@
 from pydantic import validator, BaseModel
 from src.domain.errors.errors import EntityError
+from ..enums.evaluation_type import EvaluationType
 
 
 class Grade(BaseModel):
     value: float
     idGrade: int
-    idStudent: int
-    codeSubject: str
     academicYear: int
+    evaluationType: EvaluationType
 
     @validator('academicYear')
     def acdemicYear_is_valid(cls, v: int) -> int:
         if v < 1960:
             raise EntityError('academicYear')
-        return v
-
-    @validator('idStudent')
-    def idStudent_is_not_empty(cls, v: int) -> int:
-        if v == 0:
-            raise EntityError('idStudent')
-        return v
-
-    @validator('codeSubject')
-    def codeSubject_is_not_empty(cls, v: str) -> str:
-        if len(v) == 0:
-            raise EntityError('CodeSubject')
         return v
 
     @validator('value')
---
+++ src/domain/enums/evaluation_type.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class EvaluationType(Enum):
+    P1 = 'P1'
+    P2 = 'P2'
+    P3 = 'P3'
+    P4 = 'P4'
+    PS1 = 'PS1'
+    PS2 = 'PS2'
+    T1 = 'T1'
+    T2 = 'T2'
+    T3 = 'T3'
+    T4 = 'T4'
+    T5 = 'T5'
+    T6 = 'T6'
+    T7 = 'T7'
+    T8 = 'T8'
+    T9 = 'T9'
+    T10 = 'T10'
+    T11 = 'T11'
+    T12 = 'T12'
