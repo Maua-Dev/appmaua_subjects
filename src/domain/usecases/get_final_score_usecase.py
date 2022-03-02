7d7f4edafa5d98ef6cb0b4f9e48cb7de98eb6ed5
Renan Reschke <renanreschke@hotmail.com>
2022-03-01 23:44:28 -0300

    refatoração

--- src/domain/usecases/get_final_score_usecase.py
+++ src/domain/usecases/get_final_score_usecase.py
@@ -1,6 +1,7 @@
 from src.domain.errors.errors import UnexpectedError
 from src.domain.repositories.subject_repository_interface import ISubjectRepository
 
+
 class GetFinalScoreUsecase:
 
     def __init__(self, subjectRepository: ISubjectRepository) -> None:
@@ -29,64 +30,60 @@ class GetFinalScoreUsecase:
             workQnt = await self._subjectRepository.getEvalQuantityByType(codeSubject.upper(), academicYear,
                                                                           19)  # Qnt Trabalhos
             subQnt = await self._subjectRepository.getEvalQuantityByType(codeSubject.upper(), academicYear,
-                                                                          21)  # Qnt Subs
+                                                                         21)  # Qnt Subs
 
             testScores = []
             workScores = []
             subScores = []
 
-            for i in range(1, testQnt+1):
+            partialTestWeights = []
+            partialWorkWeights = []
+
+            # Notas e pesos de cada prova
+            for i in range(1, testQnt + 1):
                 testScores.append(await self._subjectRepository.getSubjectScoreByEvalType(codeSubject.upper(),
                                                                                           idStudent,
                                                                                           academicYear,
                                                                                           i))
-                # index da lista define a prova
-                # testScores[n] = P(n+1)
-
-            for j in range(7, workQnt+7):
-                workScores.append(await self._subjectRepository.getSubjectScoreByEvalType(codeSubject.upper(),
-                                                                                          idStudent,
-                                                                                          academicYear,
-                                                                                          j))
-                # index da lista define o trabalho
-                # workScores[n] = T(n+1)
-
-            for k in range(5, subQnt+5):
-                subScores.append(await self._subjectRepository.getSubjectScoreByEvalType(codeSubject.upper(),
-                                                                                          idStudent,
-                                                                                          academicYear,
-                                                                                          k))
-                # index da lista define a sub
-                # subScores[n] = PS(n+1)
-
-            # avalia existencia de notas sub p substituir provas
-            for score in range(len(subScores)):
-                if subScores[score] is not None:
-                    testsToReplace = await self._subjectRepository.getWichScoreToReplace(codeSubject.upper(),
-                                                                                        academicYear,
-                                                                                        score+5)
-                    if testsToReplace is not None:
-                        for test in testsToReplace:
-                            if ((testScores[test-1] < subScores[score])
-                                    and (testScores[test-1] != -1)):
-                                testScores[test-1] = subScores[score]
-
-            partialTestWeights = []
-            partialWorkWeights = []
-
-            for i in range(1, testQnt+1):
                 partialTestWeights.append(await self._subjectRepository.getEvalWeightByType(codeSubject.upper(),
                                                                                             academicYear,
                                                                                             i))
                 # index da lista define a prova
-                # partialTestWeights[n] = P(n+1)
+                # testScores[n] = P(n+1)
 
-            for j in range(7, workQnt+7):
+            # Notas e pesos de cada trabalho
+            for j in range(7, workQnt + 7):
+                workScores.append(await self._subjectRepository.getSubjectScoreByEvalType(codeSubject.upper(),
+                                                                                          idStudent,
+                                                                                          academicYear,
+                                                                                          j))
                 partialWorkWeights.append(await self._subjectRepository.getEvalWeightByType(codeSubject.upper(),
                                                                                             academicYear,
                                                                                             j))
                 # index da lista define o trabalho
-                # partialWorkWeights[n] = T(n+1)
+                # workScores[n] = T(n+1)
+
+            # Notas e pesos de cada PSub
+            for k in range(5, subQnt + 5):
+                subScores.append(await self._subjectRepository.getSubjectScoreByEvalType(codeSubject.upper(),
+                                                                                         idStudent,
+                                                                                         academicYear,
+                                                                                         k))
+                # index da lista define a sub
+                # subScores[n] = PS(n+1)
+
+            """   Avalia existencia de notas sub p substituir provas (caso a sub seja maior que a prova a qual ela substitui
+              e não tenha nota -1 (cola) para essa prova)"""
+            for score in range(len(subScores)):
+                if subScores[score] is not None:
+                    testsToReplace = await self._subjectRepository.getWichScoreToReplace(codeSubject.upper(),
+                                                                                         academicYear,
+                                                                                         score + 5)
+                    if testsToReplace is not None:
+                        for test in testsToReplace:
+                            if ((testScores[test - 1] < subScores[score])
+                                    and (testScores[test - 1] != -1)):
+                                testScores[test - 1] = subScores[score]
 
             totalPartialTestWeight = sum(partialTestWeights)
             totalPartialWorkWeight = sum(partialWorkWeights)
@@ -101,7 +98,7 @@ class GetFinalScoreUsecase:
             aux = 0
             for i in range(len(testScores)):
                 if testScores[i] is not None and testScores[i] >= 0:
-                    aux += testScores[i]*partialTestWeights[i]
+                    aux += testScores[i] * partialTestWeights[i]
                 else:
                     isPartialScore = True
             testAverage = aux / totalPartialTestWeight
@@ -115,8 +112,8 @@ class GetFinalScoreUsecase:
                     isPartialScore = True
             workAverage = aux / totalPartialWorkWeight
 
-            finalAverage = (testAverage*finalTestWeight + workAverage*finalWorkWeight)/(finalTestWeight +
-                                                                                        finalWorkWeight)
+            finalAverage = (testAverage * finalTestWeight + workAverage * finalWorkWeight) / (finalTestWeight +
+                                                                                              finalWorkWeight)
             return round(finalAverage, 1), isPartialScore
         except Exception as error:
             raise UnexpectedError('GetFinalScoreUsecase', str(error))
