93d3a68e40b10b8b5efe7eb112c5808cca1beb6d
Bruno Vilardi <brunovilardibueno@gmail.com>
2022-05-23 11:12:43 -0300

    rename stack

--- IaC/app.py
+++ IaC/app.py
@@ -3,13 +3,13 @@ import os
 
 import aws_cdk as cdk
 
-from iac.iac_stack import IaCStack
+from iac.iac_stack import AppMauaBack
 
 env_TEMP = cdk.Environment(account="171487071332", region="us-east-1")
 
 
 
 app = cdk.App()
-IaCStack(app, "IaCStack", env=env_TEMP)
+AppMauaBack(app, "IaCStack", env=env_TEMP)
 
 app.synth()
