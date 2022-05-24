c981e4f7fd9e6a6c464b1473d5100e315131ec60
Bruno Vilardi <brunovilardibueno@gmail.com>
2022-05-24 19:59:05 -0300

    setup timeout for connection

--- tests/external/dynamo/test_dynamo_table.py
+++ tests/external/dynamo/test_dynamo_table.py
@@ -5,9 +5,8 @@ from src.external.dynamo.dynamo_table import DynamoTable
 
 class Test_DynamoTable:
 
-    @pytest.mark.asyncio
-    async def test_find_tables(self):
+    @pytest.mark.timeout(10)
+    def test_find_tables(self):
         table = DynamoTable()
         with table as t:
             assert t.table_status == 'ACTIVE'
-
