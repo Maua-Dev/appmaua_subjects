e72f9f1f7e2094df455a6747a7489c850ffa70b0
Bruno Vilardi <brunovilardibueno@gmail.com>
2022-05-31 21:42:02 -0300

    add shared functions

--- src/helpers/functions.py
+++ src/helpers/functions.py
@@ -1,8 +1,12 @@
-from src.domain.entities.subject import Subject
+from typing import List
 
+from src.domain.entities.grade import Grade
 
-def getCurrentGrade(subject: Subject) -> float:
+#grade.value pode ser None, se nao tiver feito a prova ainda - isso conta no Current e nao conta no partial
+
+def getCurrentGrade(grades: List[Grade]) -> float:
+
     pass
 
-def getPartialGrade(subject: Subject) -> float:
+def getPartialGrade(grades: List[Grade]) -> float:
     pass
