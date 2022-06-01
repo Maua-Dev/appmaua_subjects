1eaa89e9a66c382491e8443e8fbd3c4359c370f4
João Vitor <21.01075-7@maua.br>
2022-06-01 12:34:09 -0300

    getCurrentGrade e getPartialGrade, sem seu teste unitário

--- src/helpers/functions.py
+++ src/helpers/functions.py
@@ -5,8 +5,19 @@ from src.domain.entities.grade import Grade
 #grade.value pode ser None, se nao tiver feito a prova ainda - isso conta no Current e nao conta no partial
 
 def getCurrentGrade(grades: List[Grade]) -> float:
+    current_grade = 0
+    for grade in grades:
+        if grade.value != None: #se for None, eu somaria à current_grade 0
+            current_grade += grade.value * grade.weight
 
-    pass
+    return current_grade
 
 def getPartialGrade(grades: List[Grade]) -> float:
-    pass
+    partial_grade = 0
+    partial_weight = 0
+    for grade in grades:
+        if grade.value != None:
+            partial_grade += grade.value * grade.weight
+            partial_weight += grade.weight
+
+    return partial_grade/partial_weight
