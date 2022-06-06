3f12a2fec86706294a1208899d009179d64e2654
leonardozollner <79452460+leonardozollner@users.noreply.github.com>
2022-06-05 22:10:10 -0300

    Update degree_enum.py

--- src/domain/enums/degree_enum.py
+++ src/domain/enums/degree_enum.py
@@ -4,12 +4,19 @@ from enum import Enum
 class DegreeEnum(Enum):
     ADM = "Administração"
     DSG = "Design"
+    EFB = "Engenharia Ciclo Básico
+    ETE = "Engenharia Tronco Eletrica"
+    ETM = "Engenharia Tronco Mecânica
+    ETQ = "Engenharia Tronco Química"
+    TTI = "Tronco Tecnologias da Informação"
+    EAL = "Engenharia de Alimentos"
     ETC = "Engenharia Civíl"
     EMC = "Engenharia Mecânica"
-    EAL = "Engenharia de Alimentos"
     EQM = "Engenharia Química"
     EPM = "Engenharia de Produção"
     ECA = "Engenharia de Controle e Automação"
-    EEE = "Engenharia Elétrica"
+    EET= "Engenharia Elétrica"
     EEN = "Engenharia Eletrônica"
     ECM = "Engenharia de Computação"
+   
+    
