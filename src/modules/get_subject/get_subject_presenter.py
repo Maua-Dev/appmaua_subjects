824243718d8a4fb5ee0258808b4ed78bfebd09c4
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2022-07-04 10:07:13 -0300

    test_get_subject_presenter e get_subject_presenter

--- src/modules/get_subject/get_subject_presenter.py
+++ src/modules/get_subject/get_subject_presenter.py
@@ -1,11 +1,21 @@
 import asyncio
 
+from src.helpers.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
 from src.infra.repositories.subject_repository_dynamo import SubjectRepositoryDynamo
+from src.infra.repositories.subject_repository_mock import SubjectRepositoryMock
+from src.modules.get_subject.get_subject_controller import GetSubjectController
 from src.modules.get_subject.get_subject_usecase import GetSubjectUsecase
 
-repo = SubjectRepositoryDynamo()
-
-usecase = GetSubjectUsecase(repo)
 
 
-print(asyncio.run(usecase("17.00163-3", "ECM502")))
+async def lambda_handler(event, context):
+    #repo = SubjectRepositoryDynamo()
+    repo = SubjectRepositoryMock()
+    usecase = GetSubjectUsecase(repo)
+    controller = GetSubjectController(usecase)
+
+    httpRequest = LambdaHttpRequest(data=event)
+    response = await controller(httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+
+    return httpResponse.toDict()
